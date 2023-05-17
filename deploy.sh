bfc3fbe7db11e205864d632e83d0913640108815
izemaghilas <saidtala23@gmail.com>
2023-05-17 23:27:45 +0200

    fix permission denied

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ if [[ -n "$SSH_PRIVATE_KEY" ]]; then
   echo "$SSH_PRIVATE_KEY" | tr -d '\r' | ssh-add - > /dev/null
 fi
 
-# Copy known hosts into the SSH config.
+# Copy known hosts into the SSH config..
 mkdir -p ~/.ssh && chmod 0700 ~/.ssh
 cat ${GITHUB_ACTION_PATH}/known_hosts >> ~/.ssh/known_hosts
 
